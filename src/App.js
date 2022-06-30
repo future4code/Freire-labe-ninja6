df528cdb36d605a609bccb9f64840af2c7c32e45
Arlindo Vinícius Amado Nascimento Silva <104537653+vinyperroni@users.noreply.github.com>
2022-06-30 14:28:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 
-import logo from './logo.svg';
-import './App.css';
-import './componentes/Inscricao'
-import Inscricao from './componentes/Inscricao';
-import Detalhes from './componentes/Detalhes';
-
-
-
 function App() {
   return (
     <div>
       oi
-
     </div>
   );
 }
