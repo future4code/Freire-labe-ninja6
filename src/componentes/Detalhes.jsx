c2e4987fe8d8505e7830da49030851d8657996d1
carolfortuna <63572811+carolfortuna@users.noreply.github.com>
2022-06-30 14:23:00 -0300

    componentes formulario inscrição serviços e componente detalhes

---
+++ src/componentes/Detalhes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Detalhes extends React.Component{
+    render(){
+        return(
+            <center>
+                <h2>Titulo</h2>
+                <p>Preço</p>
+                <p>Prazo:</p>
+                <p>Descrição</p>
+                <p>formas de pagamento</p>
+                <button>Voltar para lista</button>
+            </center>
+
+        )
+    }
+}
