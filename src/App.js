aa44bc585bf4c7e7d9fa3de4424bcebe4eb21a31
petersonmaia <peterson.maia20@gmail.com>
2022-07-02 15:34:58 -0300

    Ajuste de conflitos

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Home from './components/Header-Footer-Home/Home'
+import HeaderEFooter from './components/Header-Footer-Home/HeaderEFooter'
+
+
+
+export default class App extends React.Component{
+
+
+render(){
+
+  return(
+
+{/* <>
+<HeaderEFooter/>
+<Home/>
+</> */}
+
+  )
+}
+}
