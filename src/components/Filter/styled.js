7fb713b9ea6df34cb9359deaa094069088f8528d
João Gabriel Guimarães <joaoguimaraes14@gmail.com>
2022-07-03 13:33:12 -0300

    fazendo estilização filtro

--- src/components/Filter/styled.js
+++ src/components/Filter/styled.js
@@ -1,37 +1,16 @@
 import styled from "styled-components";
 
-export const Input = styled.input`
-  height: 25px;
-  padding-left: 8px;
-  margin-right: 15px;
-  border: 1px solid lightgray;
-  border-radius: 3px;
-  margin-bottom: 10px;
-  &:hover {
-    border-color: black;
-  }
-  &:focus {
-    outline: none;
-    border-color: lightsalmon;
-  }
-`;
-export const Select = styled.select`
-  height: 29px;
-  border-radius: 3px;
-  border: 1px solid lightgray;
-  &:hover {
-    border-color: black;
-    cursor: pointer;
-  }
-  &:focus {
-    outline: none;
-    border-color: lightsalmon;
-  }
-`;
-
 export const Div = styled.div`
   display: flex;
   margin-bottom: 20px;
-  justify-content: center;
+  justify-content: space-around;
+  padding: 0 25vw 0 25vw;
   margin-top: 10px;
+  @media screen and (min-device-width: 320px) and (max-device-width: 860px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 5px;
+    height: 200px;
+  }
 `;
