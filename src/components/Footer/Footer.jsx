f790403c3df7e9c9cbcf2eeea6b15760c6b7be50
Pablo Leão <pablo_leao@hotmail.com>
2022-07-03 20:00:02 -0300

    Adicionado linkedin que faltava

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -89,7 +89,7 @@ export default class HeaderEFooter extends React.Component {
             <Imagem href="https://www.linkedin.com/in/carolina-fortuna/" target="_blank"><img width='40px' src='https://media-exp2.licdn.com/dms/image/C4E03AQFaJHA6_2fu8A/profile-displayphoto-shrink_200_200/0/1579930268738?e=1662595200&v=beta&t=CVSW-ZFWVZFtMrNrYVU_sSxLcur6vSKPa-C8mbfrS90' alt="" /></Imagem>
             <Imagem href="https://www.linkedin.com/in/pablo-leao/" target="_blank"><img width='40px' src='https://media-exp2.licdn.com/dms/image/C5603AQHoRy_hhnD87A/profile-displayphoto-shrink_200_200/0/1621551128951?e=1662595200&v=beta&t=p4ZuY6rfUc0mbURJHcMuNADILuwHH5qXuQhHTFzZ0Qs' alt="" /></Imagem>
             <Imagem href="https://www.linkedin.com/in/joao-gabriel-guimaraes-7aa246191/" target="_blank"><img width='40px' src='https://media-exp2.licdn.com/dms/image/C4D03AQGM1pCncZVg0w/profile-displayphoto-shrink_200_200/0/1651504824858?e=1662595200&v=beta&t=1KVHdBJLDEoGHZSZgiqsOeNZerP0Hs7RrWWyFicL_0o' alt="" /></Imagem>
-            <Imagem href="" target="_blank"><img width='40px' src='https://media-exp1.licdn.com/dms/image/C4E03AQF5FCU8ud5VuQ/profile-displayphoto-shrink_200_200/0/1624194324042?e=1661990400&v=beta&t=aU4aVRjFGNDdp4iAPCsR7P9ssvTrHrphOagunlFqP1o' alt="" /></Imagem>
+            <Imagem href="https://www.linkedin.com/in/arlindo-vin%C3%ADcius-amado-nascimento-silva-714079184/" target="_blank"><img width='40px' src='https://media-exp2.licdn.com/dms/image/C4E03AQHqHBoYpFy7Fw/profile-displayphoto-shrink_400_400/0/1656888957972?e=1662595200&v=beta&t=PZ3ACysqBwMMl5K513JJcfVwXCXDIB0XJU3hacp-JpM' alt="" /></Imagem>
           </ImgLink>
 
           <RedesSociais>
